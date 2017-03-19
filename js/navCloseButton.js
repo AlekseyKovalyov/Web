8912d559ad8c762811448f8e268697365da743da
AlekseyKovalyov <vkontakte598022@mail.ru>
2017-03-19 18:16:34 +0200

    Navigation panel sliding finished

--- js/navCloseButton.js
+++ js/navCloseButton.js
@@ -2,6 +2,8 @@
  * Created by aleksey on 3/19/17.
  */
 $(document).ready(function () {
+
+
     $(".nav_close_button").mouseover(function () {
         $("#img_nav_close_reg").fadeOut(500);
 
@@ -16,6 +18,18 @@ $(document).ready(function () {
 
         $("#img_nav_close_hover").fadeOut(500);
     })
+
+
+    $(".nav_close_button").click(function () {
+        $("#navigation_panel").animate({"left": '-=350'}, 1000);
+        $("#navigation_open").animate({"left": '+=100'}, 1000);
+    })
+
+    $("#nav_open_button").click(function () {
+        $("#navigation_panel").animate({"left": '+=350'}, 1000);
+        $("#navigation_open").animate({"left": '-=100'}, 1000);
+    })
+
 
 })
 
