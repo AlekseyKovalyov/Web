1d023c99b855cee74ddcaf265dcb3b5e8a8f6c57
AlekseyKovalyov <vkontakte598022@mail.ru>
2017-03-21 16:09:01 +0200

    login page improvment

--- js/login.js
+++ js/login.js
@@ -4,11 +4,13 @@
 $(document).ready(function () {
 
     $("#submit_login").mouseover(function () {
-        $("#submit_login").animate({backgroundColor : "#5f5d5d"}, 200)
+        $("#submit_login").animate({backgroundColor : "#5f5d5d"}, 200);
+        $ ("#submit_login_text").animate({color : "black"}, 200);
     });
 
 
     $("#submit_login").mouseout(function () {
-        $("#submit_login").animate({backgroundColor : "black"}, 200)
+        $("#submit_login").animate({backgroundColor : "black"}, 200);
+        $ ("#submit_login_text").animate({color : "white"}, 200)
     });
 });
